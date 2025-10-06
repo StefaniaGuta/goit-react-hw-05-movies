497db9bac9b860d4f674107da797ab3413b6b5bc
StefaniaFlorinela <stefania.florinela@yahoo.com>
2025-10-06 16:24:05 +0300

    finish mainPage

---
+++ src/components/MainPoster/MainPoster.jsx
@@ -0,0 +1,98 @@
+import { useEffect, useState} from "react";
+import {  newSeriesFetch } from "../../redux/series/seriesApi";
+import { useDispatch } from "react-redux";
+import star from '../../components/Images/star.png';
+import calendar from '../../components/Images/calendar.png';
+import { useNavigate } from "react-router-dom";
+import Trailers from "components/Trailer/Trailer";
+import "./MainPoster.css";
+
+const MainPoster = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [main, setMain] = useState([]);
+  const [highlight, setHighlight] = useState(null);
+  const [index, setIndex] = useState(0);
+
+ 
+  useEffect(() => {
+    const fetchSeries = async () => {
+      const ser = await dispatch(newSeriesFetch());
+      if (ser?.payload?.length) {
+        setMain(ser.payload);
+        setHighlight(ser.payload[0]);
+      }
+    };
+    fetchSeries();
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!main.length) return;
+
+    const interval = setInterval(() => {
+      setIndex((prev) => {
+        const newIndex = (prev + 1) % main.length;
+        setHighlight(main[newIndex]);
+        return newIndex;
+      });
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, [main]);
+
+  console.log(index)
+
+  return (
+    <section className="mainPosterSection">
+      {highlight ? (
+        <div className="imageWrapper">
+          <img
+            className="mainImgCover"
+            src={
+              "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces" +
+              highlight.backdrop_path
+            }
+            alt={highlight.name}
+          />
+          <div className="movieInfos">
+            <div className="movieButtons">
+              <button type="button" onClick={() => navigate(`/login`)}>
+                Watch now
+              </button>
+              <Trailers id={highlight} />
+            </div>
+            <div>
+              <h1 className="movieTitle">{highlight.name}</h1>
+              <div className="gen_rating_time">
+                <ul className="movieGen">
+                  {highlight.genres.map(g => (
+                    <li key={g.id}>{g.name}</li>
+                  ))}
+                </ul>
+                <span className="rating">
+                  <img src={star} alt="star" />
+                  {highlight.vote_average?.toFixed(1)}
+                </span>
+                <span>
+                  <img src={calendar} alt="calendar" />
+                  {new Date(highlight.first_air_date).getFullYear()}
+                </span>
+              </div>
+              <p className="movieOverview">{highlight.overview}</p>
+              <ul className="movieSeasons">
+                {highlight.seasons?.slice(0, 3).map((s) => (
+                  <li key={s.id}>{s.name}</li>
+                ))}
+                {highlight.seasons.length > 3 ? <li className="moSeasonsBtn">More Seasons</li> : null}
+              </ul>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="imageWrapper"></div>
+      )}
+    </section>
+  );
+};
+
+export default MainPoster;
--- src/components/pages/MainPAge/MainPage.jsx
+++ src/components/pages/MainPAge/MainPage.jsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from "react";
-import { getMostPopular, day, getGenres, popularActors, IMAGE_URL } from "../../../redux/movies/getAPI";
-import { seriesDetails } from "../../../redux/series/seriesApi";
+import { useEffect, useState, useRef } from "react";
+import { getMovies, getMostPopular, day, getGenres, popularActors, IMAGE_URL } from "../../../redux/movies/getAPI";
+
 import { useDispatch } from "react-redux";
 import SphereScroll from "components/SphereScroll/SphereScroll";
-import { useNavigate } from "react-router-dom";
-import star from '../../Images/star.png';
-import calendar from '../../Images/calendar.png';
-import Trailers from "components/Trailer/Trailer";
 import url from '../../Images/icons.svg';
+import MainPoster from "components/MainPoster/MainPoster";
 import "./MainPage.css";
 
 const MainPage = () => {
   const dispatch = useDispatch();
   const [recentMovie, setRecentMovie] = useState([]);
-  const [mainImag, setMainImag] = useState();
   const [genres, setGenres] = useState([]);
-  const [actors, setActors] = useState([])
-  const navigate = useNavigate();
+  const [actors, setActors] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const ref = useRef(null);
+  const [scrollAmount, setScrollAmount] = useState(0);
 
  useEffect(() => {
   const fetchMostRecentMovie = async () => {
@@ -25,8 +23,7 @@ const MainPage = () => {
       const moviesFiltered = response.payload.results.filter(m => m.poster_path);
         setRecentMovie(moviesFiltered);
 
-      const res = await dispatch(seriesDetails("194766"));
-      setMainImag(res.payload)
+     
       
     } catch (e) {
       console.error("Eroare la fetch:", e);
@@ -41,7 +38,6 @@ const MainPage = () => {
       console.log(e)
     }
   }
-
   const fetchActors = async () => {
     try{
       const res = await dispatch(popularActors());
@@ -50,76 +46,63 @@ const MainPage = () => {
       console.log(e)
     }
   }
+  const fetchTrending = async () => {
+    const response = await dispatch(getMovies())
+    const sorted = response.payload.results
+      .filter(m => m.poster_path || m.backdrop_path)
+      .sort((a, b) => b.vote_average - a.vote_average)
+      .slice(0, 3);
+    
+    setTrending(sorted);
+  }
+const updateScrollAmount = () => {
+      const width = window.innerWidth;
+      setScrollAmount(Math.floor(width));
+    };
 
-  fetchMostRecentMovie();
-  fetchGenres();
-  fetchActors();
+    updateScrollAmount();
+    window.addEventListener("resize", updateScrollAmount);
+    fetchMostRecentMovie();
+    fetchGenres();
+    fetchActors();
+    fetchTrending();
+    return () => window.removeEventListener("resize", updateScrollAmount);
 }, [dispatch]);
 
+const scroll = (dir) => {
+    if (!ref.current) return;
+    ref.current.scrollBy({
+      left: dir === "left" ? -scrollAmount : scrollAmount,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <section className="mainSection">
-
-    {mainImag ? (
-      <div className="imageWrapper">
-          <img className="mainImgCover"
-              src={"https://image.tmdb.org/t/p/w1920_and_h800_multi_faces" + mainImag.backdrop_path}
-              alt={mainImag.name}
-          />
-          <div className="movieInfos">
-            <div className="movieButtons">
-              <button type="button" onClick={() => navigate(`/login`)}>Watch now</button>
-              <Trailers id={mainImag}/>
-            </div>
-            <div>
-              <h1 className="movieTitle">{mainImag.name}</h1>
-              <div className="gen_rating_time">
-                <p className="movieGen">{mainImag.genres[0].name}</p>
-                <span className="rating">
-                  <img src={star} alt="star"/>
-                  {mainImag.vote_average.toFixed(1)}
-                </span>
-                <span>
-                  <img src={calendar} alt="calendar"/>
-                  {new Date(mainImag.first_air_date).getFullYear()}
-                </span>
-              </div>
-              <p className="movieOverview">{mainImag.overview}</p>
-              <ul className="movieSeasons">
-                {mainImag.seasons.map(s => (
-                  <li key={s.id}>
-                    {s.name}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            
-          </div>
+       <MainPoster/>
+      <div className="genresSection">
+        <div>
+          <h2 className="sectionTitle">choose the type of film you liked</h2>
+          <p className="sectionText">We present many films from various main categories, let's chooose and search film of you liked</p>
         </div>
-    ) : (<p></p>)}
-
-    <div className="genresSection">
-      <div>
-        <h2 className="sectionTitle">choose the type of film you liked</h2>
-        <p className="sectionText">We present many films from various main categories, let's chooose and search film of you liked</p>
-      </div>
-      <ul className="genresList">
-        {genres.map(gen => (
-          <li key={gen.id}>
-            <svg width="29" height="32"><use xlinkHref={`${url}#${gen.name}`}/></svg>
-            <span>
-            {gen.name}
-            <a href="/">View more</a>
-            </span>
+        <ul className="genresList">
+          {genres
+          .map(gen => (
+            <li key={gen.id}>
+              <svg width="29" height="32"><use xlinkHref={`${url}#${gen.name}`}/></svg>
+              <span>
+                {gen.name}
+                <a href="/">View more</a>
+              </span>
             </li>
-        ))}
-      </ul>
-    </div>
+          ))}
+        </ul>
+      </div>
 
-    <div className="devicesSection">
+      <div className="devicesSection">
         <h2 className="title">Any time, Anywhere</h2>
         <p className="text">Watch your favorite movie or TV series on all types of devices<br></br>
-        Stream and enjoy your favorites on your device
+          Stream and enjoy your favorites on your device
         </p>
         <ul className="devicesList">
           <li>
@@ -135,24 +118,61 @@ const MainPage = () => {
             Tablet & Mobil
           </li>
         </ul>
-    </div>
+      </div>
 
-      <h2>Most popular celebrities</h2>
-      <ul className="actorsList">
-        {actors.map(a => (
-          <li key={a.id}>
-            <img src={IMAGE_URL + a.profile_path}
-            alt={a.name}
-            />
-            <h3>{a.name}</h3>
-            <p>{a.known_for_department}</p>
+      <div className="popularCelebritiesSection">
+        <h2>Most popular celebrities</h2>
+        <div className="moveBtn">
+          <button type="button" onClick={() => scroll("left")}>
+            &lsaquo;
+          </button>
+          <button type="button" onClick={() => scroll("right")}>
+            &rsaquo;
+          </button>
+        </div>
+        <ul className="actorsList" ref={ref}>
+          {actors.map(a => (
+            <li key={a.id}>
+              <img src={IMAGE_URL + a.profile_path} alt={a.name}/>
+              <h3>{a.name}</h3>
+              <p>{a.known_for_department}</p>
             </li>
-          
-        ))}
-      </ul>
+          ))}
+        </ul>
+      </div>    
 
-    <h2>What's Popular</h2> 
-    <SphereScroll  movies={recentMovie}/>
+      <div className="trendingSection">
+        <h2>Top 3 the most wanted movies this week</h2>
+        <ul className="trendingList">
+          {trending 
+            .map(t => (
+              <li key={t.id} className="recentMovie" to={`/movies/${t.id}`}>
+                <svg viewBox="0 0 300 150" className="titleArc">
+                  <defs>
+                    <path id={`arcPath-${t.id}`} d="M 20 130 A 130 130 0 0 1 280 130" />
+                  </defs>
+                  <text fill="white" fontSize="16" fontWeight="bold">
+                    <textPath href={`#arcPath-${t.id}`} startOffset="50%" textAnchor="middle">
+                      {t.title || t.name}
+                    </textPath>
+                  </text>
+                </svg>
+                <img
+                  src={IMAGE_URL + t.poster_path || t.backdrop_path
+                  }
+                  alt={t.title || t.name}
+                />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className="isPopularSection">
+        <h2>What's Popular</h2> 
+        <SphereScroll  movies={recentMovie}/>
+      </div>
+
     </section>
   );
 };
